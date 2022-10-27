9b44b96fe95dcde07ac05b7d906eb98e0e709fcb
Arthur Casey <57399280+xStar2222@users.noreply.github.com>
2022-10-27 17:51:56 -0400

    Add files via upload

---
+++ random cord gen.py
@@ -0,0 +1,12 @@
+import random
+from colorama import Fore, Back, Style
+
+print(Fore.GREEN + 'Here Are Your Cords! Enjoy!')
+
+print(".......")
+
+value = random.uniform(0.00000000000, 99.99999999999)
+print(value)
+
+value = random.uniform(0.00000000000, 99.99999999999)
+print(value)
